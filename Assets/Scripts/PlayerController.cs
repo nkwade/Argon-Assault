c2b44b64a60420ae8f7f1ccd34a6b774324b917e
beakerwade <beaker.wade@gmail.com>
2022-03-15 16:05:45 -0400

    create movement rail and added laser

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,34 +6,63 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private InputAction movment;
+    [SerializeField] private InputAction movement;
 
+    //Speed for x and y movement
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float ySpeed;
+    //range for where the user can move the spaceship
+    [SerializeField] private float xRange;
+    [SerializeField] private float yRange;
+    //factors based off the current user position used in calculating rotation
+    [SerializeField] private float posPitchFactor = 2f;
+    [SerializeField] private float posYawFactor = 2f;
+    //factors based off the the current user input using in calculatin rotatio
+    [SerializeField] private float inputPitchFactor = 10f;
+    [SerializeField] private float inputRollFactor = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //the current factor of the user pressing down the movement controls
+    float horizontalThrow;
+    float verticalThrow;
+    
 
     private void OnEnable() {
-        movment.Enable();
+        movement.Enable();
     }
-
     private void OnDisable() {
-        movment.Disable();
+        movement.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Old Unity Movement Code
-        // float horizontalThrow = Input.GetAxis("Horizontal");
-        // float verticalThrow = Input.GetAxis("Vertical");
+        ProcessMovement();
+        ProcessRotation();
 
-        float horizontalThrow = movment.ReadValue<Vector2>().x;
-        float verticalThrow = movment.ReadValue<Vector2>().y;
+    }
 
 
+    private void ProcessMovement()
+    {
+        horizontalThrow = movement.ReadValue<Vector2>().x;
+        verticalThrow = movement.ReadValue<Vector2>().y;
 
+        float newX = transform.localPosition.x + (horizontalThrow * Time.deltaTime * xSpeed);
+        float newY = transform.localPosition.y + (verticalThrow * Time.deltaTime * ySpeed);
+
+        float xClamp = Mathf.Clamp(newX, -xRange, xRange);
+        float yClamp = Mathf.Clamp(newY, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(xClamp, yClamp, transform.localPosition.z);
+    }
+    
+    private void ProcessRotation()
+    {
+        float pitch = (transform.localPosition.y * posPitchFactor) + (verticalThrow * inputPitchFactor); //pitch according to pos plus pitch according to the player pushing up
+        float yaw = transform.localPosition.x * posYawFactor; //yaw according to current position on screen
+        float roll = horizontalThrow * inputRollFactor; //roll according to current input from player
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
