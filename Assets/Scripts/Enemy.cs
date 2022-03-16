996ffdcb3c47ebc2adf5d61c54d919270c9ba259
beakerwade <beaker.wade@gmail.com>
2022-03-16 11:09:04 -0400

    all enemies have rigidbody and created scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,41 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject explosion;
+    [SerializeField] private GameObject hit;
     [SerializeField] private Transform parent;
-    private void OnParticleCollision(GameObject other) {
+    [SerializeField] private int pointValue = 1;
+    [SerializeField] private int hitPoints = 2;
+
+    private Scoreboard scoreboard;
+
+    private void Start() {
+        scoreboard = FindObjectOfType<Scoreboard>();
+        //makes sure all enemies have rigidbodies in order to get collision within child objects
+        Rigidbody rb  = this.gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 0) {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
         GameObject vfx = Instantiate(explosion, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(this.gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hit, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+
+        hitPoints--;
+        scoreboard.IncreaseScore(pointValue);
     }
 }
--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    int score;
+    TMP_Text scoreText;
+
+    private void Start() {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = $"Score : {score}";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void IncreaseScore(int increase) {
+        score += increase;
+        scoreText.text = $"Score : {score}";
+    }   
 }
